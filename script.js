aefe547e52b66e893470a82114dfb9cb12b5cd73
AryaJF <aryajavasfatih888@gmail.com>
2022-09-05 11:23:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const hexColor = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+const button = document.getElementById('changeButton')
+console.log(button)
+const color = document.getElementById('color')
+
+function changeColor(){
+    let warna = '#'
+    alert('Mantappp')
+}
